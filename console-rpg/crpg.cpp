a7808e512e26d006b898aaecf6534e4e81701f93
Mike Polan <mbpolan@gmail.com>
2004-10-19 19:50:16 +0000

    0.1.8 release

--- console-rpg/crpg.cpp
+++ console-rpg/crpg.cpp
@@ -4,6 +4,6 @@
 
 main(int argc, char *argv[]) {
 	player *Player=new player(500,200,6,18); // player HP/MP/Height(ft)/age
-	std::cout << "\nConsole-RPG 0.1.5\n\n" << std::endl;
+	std::cout << "\nConsole-RPG 0.1.8\n\n" << std::endl;
 	menu(Player); // call menu() and pass Player in
 }
